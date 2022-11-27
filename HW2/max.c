4ca58402b59f22d7b8c64bef701220d6efd6fdb6
IdoSheinman <118105901+IdoSheinman@users.noreply.github.com>
2022-11-27 19:46:34 +0200

    Update max.c

--- HW2/max.c
+++ HW2/max.c
@@ -5,6 +5,12 @@ enum { MIN_GRADE=0, MAX_GRADE=100 };
 
 void operate(FILE *f);
 
+/**
+ * @brief Accepts inputs from file or stdin.
+ * @param argc Number of inputs.
+ * @param argc Inputs as strings.
+ * @note ?? .
+ */
 int main(int argc, char **argv) {
 	FILE *f;
 
@@ -13,7 +19,7 @@ int main(int argc, char **argv) {
 	} else {
 		f = fopen(argv[1], "r");
 	}
-	/* err check */
+	/* error file check */
 	if (!f ) {
 		fprintf(stderr,"File note found: \"%s\"\n", argv[1]);
 		return 1;
@@ -21,7 +27,11 @@ int main(int argc, char **argv) {
 	operate(f);
 }
 
-
+/**
+ * @brief Found the maximum grade.
+ * @param *f file pointer.
+ * @note ?? .
+ */
 void operate(FILE *f) {
     int max;
     int grade;
@@ -30,26 +40,32 @@ void operate(FILE *f) {
     
     max = MIN_GRADE-1;
     line_n = 0;
+	
+	// scan all the grade, check validity, and save the min grade 
     while (1) {
 		retval = fscanf(f, "%d", &grade);
 		if (retval == EOF) {
 			break;
+	
+		/* check error - not a number */
 		} else if (retval != 1) {
-			/* err - not a number */
 			fprintf(stderr, "Error: In line %d: not a number \n", line_n+1);
 			exit(1);
 		}
+		/* check error - grade range */
 		else if (grade > MAX_GRADE|| grade < MIN_GRADE) {
-			/* err - illegal grade */
 			fprintf(stderr, "Error: In line %d: illegal grade \n", line_n+1);
 			exit(1);
 		}
+		/* check if this minimum grade */
 		else if (grade > max) {
 		    max = grade;
 		}
 		line_n++;
     }
 	
+	/* if not grade input the minimum will be under
+		the min legal grade */
 	if (max < MIN_GRADE) {
 	    printf ("No grades inputed.");
 	}
